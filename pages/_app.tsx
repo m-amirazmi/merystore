e45eccc60c57f608792d6186302ac2a1915d9c54
Muhamad Amir <m.amirazmi@hotmail.com>
2023-11-23 19:42:52 +0800

    Basic nextauth with google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import "@/styles/globals.css";
 import { IPageWithLayout } from "@/types/IPageWithLayout";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
 interface IAppProps extends AppProps {
   Component: IPageWithLayout;
 }
 
-export default function App({ Component, pageProps }: IAppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: IAppProps) {
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  const component = (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+  return getLayout(component);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,15 @@ import LayoutMain from "@/components/layouts/layout-main";
 import { prisma } from "@/lib/prisma";
 import { IPageWithLayout } from "@/types/IPageWithLayout";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { useSession } from "next-auth/react";
 
-interface IProps {
-  repo: IRepo[];
-}
+interface IProps {}
 
-interface IRepo {
-  id: number;
-  title: string;
-  content?: string | null;
-}
+const Page: IPageWithLayout<IProps> = () => {
+  const session = useSession();
+  console.log(session);
 
-const Page: IPageWithLayout<IProps> = ({ repo }) => {
-  return (
-    <div>
-      {repo.map((i) => (
-        <div key={i.id}>
-          {i.title} <p>{i.content}</p>
-        </div>
-      ))}
-    </div>
-  );
+  return <div></div>;
 };
 
 Page.getLayout = (page: React.ReactElement) => {
@@ -31,11 +19,11 @@ Page.getLayout = (page: React.ReactElement) => {
 
 export default Page;
 
-export const getServerSideProps: GetServerSideProps<{ repo: IRepo[] }> = async (
-  context: GetServerSidePropsContext
-) => {
-  const post = await prisma.post.findMany();
-  const repo: IRepo[] = post;
+// export const getServerSideProps: GetServerSideProps<{ repo: IRepo[] }> = async (
+//   context: GetServerSidePropsContext
+// ) => {
+//   const post = await prisma.post.findMany();
+//   const repo: IRepo[] = post;
 
-  return { props: { repo } };
-};
+//   return { props: { repo } };
+// };
